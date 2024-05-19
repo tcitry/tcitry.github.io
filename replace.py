31b507d1f2ad5db0209a4a47009b83dc0665ee5b
tcitry <tcitry@gmail.com>
2024-05-19 15:51:26 +0800

    get value from parameter to make script more generic

--- replace.py
+++ replace.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 def replace_string_in_file(file_path, old_string, new_string):
     with open(file_path, 'r') as file:
@@ -19,11 +20,15 @@ def replace_string_in_folder(folder_path, old_string, new_string, skip_folder, s
                 file_path = os.path.join(root, name)
                 replace_string_in_file(file_path, old_string, new_string)
 
-folder_path = '/Users/yindongliang/Library/Mobile Documents/com~apple~CloudDocs/Blog'  # 获取当前工作目录
 old_string = 'tcitry/Pictures/master'
 new_string = 'tcitry/static/master'
 skip_folder = '.trash'  # 指定要跳过的文件夹
 script_name = os.path.basename(__file__)  # 获取当前脚本的文件名
+
+if sys.argv[1] == '--path':
+    folder_path = sys.argv[2]
+else:
+    folder_path = os.getcwd()
 
 replace_string_in_folder(folder_path, old_string, new_string, skip_folder, script_name)
 
